32aa7477e928baabd713407507a5b51e9c96cac8
Huang Liang <exceedhl@gmail.com>
2012-03-30 16:28:38 +1100

    added publish scp and cd update

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
-emacs -q -batch -l ~/.emacs.d/muse-init.el -f muse-project-batch-publish --force blog
+emacs -q -batch -l ~/.emacs.d/muse-init.el -f muse-project-batch-publish --force Blog
 ditto blog output/blog
 ditto output/posts output/blog/_posts
 jekyll output/blog output/site
+
+scp -r output/site/* lhuang@hl.thoughtworkers.org:~/hl.thoughtworkers.org/
